f740bb62bce28249dcdf8d3cfb9e5bd2afd3dc32
IngvarListard <vlzvoice@ya.ru>
2020-09-23 23:43:52 +0300

    exercise

--- pkg/calc21/calc.go
+++ pkg/calc21/calc.go
@@ -122,39 +122,43 @@ func (i *Interpreter) consume(tokenTyp tokenType) {
 }
 
 func (i *Interpreter) Eval() int {
-	i.currentToken = i.getNextToken()
+	var result int
+	for {
+		i.currentToken = i.getNextToken()
 
-	left := i.currentToken
-	i.consume(Integer)
+		left := i.currentToken
+		i.consume(Integer)
 
-	op := i.currentToken
+		op := i.currentToken
 
-	switch op.typ {
-	case Plus:
-		i.consume(Plus)
-	case Minus:
-		i.consume(Minus)
-	case Mul:
-		i.consume(Mul)
-	case Div:
-		i.consume(Div)
-	default:
-		panic("unknown operator")
+		switch op.typ {
+		case Plus:
+			i.consume(Plus)
+		case Minus:
+			i.consume(Minus)
+		case Mul:
+			i.consume(Mul)
+		case Div:
+			i.consume(Div)
+		default:
+			panic("unknown operator")
+		}
+
+		right := i.currentToken
+		i.consume(Integer)
+
+		switch op.typ {
+		case Plus:
+			return left.val + right.val
+		case Minus:
+			return left.val - right.val
+		case Mul:
+			return left.val * right.val
+		case Div:
+			return left.val / right.val
+		default:
+			panic("unknown operand")
+		}
 	}
-
-	right := i.currentToken
-	i.consume(Integer)
-
-	switch op.typ {
-	case Plus:
-		return left.val + right.val
-	case Minus:
-		return left.val - right.val
-	case Mul:
-		return left.val * right.val
-	case Div:
-		return left.val / right.val
-	default:
-		panic("unknown operand")
-	}
+	return result
 }
