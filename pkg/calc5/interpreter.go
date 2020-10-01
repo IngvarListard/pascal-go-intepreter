d4f7f4cc468d7faab1357169b145ded56c9aa608
IngvarListard <vlzvoice@ya.ru>
2020-10-01 23:37:44 +0300

    exercise reverse polish notation

--- pkg/calc5/interpreter.go
+++ pkg/calc5/interpreter.go
@@ -1,6 +1,9 @@
 package calc5
 
-import "fmt"
+import (
+	"fmt"
+	"strconv"
+)
 
 type Interpreter struct {
 	parser *Parser
@@ -11,6 +14,13 @@ func (i *Interpreter) interpret() interface{} {
 	//v, _ := node.Value()
 	//return v
 	return i.VisitNode(node)
+}
+
+func (i *Interpreter) interpretPolish() interface{} {
+	node := i.parser.parse()
+	//v, _ := node.Value()
+	//return v
+	return i.VisitNodePolish(node)
 }
 
 func (i *Interpreter) visitBinOp(binary *BinOp) interface{} {
@@ -32,6 +42,16 @@ func (i *Interpreter) visitBinOp(binary *BinOp) interface{} {
 	}
 }
 
+/*
+значит нужно написать так чтобы скобки опускались, а знак
+
+(2+3) * (4 + 5)
+2 3 + 4 * 5 +
+
+(2+3) * 7
+2 3 + 7 *
+*/
+
 func (i *Interpreter) visitNum(num *Num) interface{} {
 	return num.value
 }
@@ -44,5 +64,50 @@ func (i *Interpreter) VisitNode(node Node) interface{} {
 		return i.visitNum(v)
 	default:
 		panic(fmt.Sprintf("unexpected type occurrence %T", v))
+	}
+}
+
+func (i *Interpreter) VisitNodePolish(node Node) interface{} {
+	switch v := node.(type) {
+	case *BinOp:
+		return i.visitBinOpPolish(v)
+	case *Num:
+		return i.visitNum(v)
+	default:
+		panic(fmt.Sprintf("unexpected type occurrence %T", v))
+	}
+}
+
+func (i *Interpreter) visitBinOpPolish(binary *BinOp) interface{} {
+	var leftStr string
+	var rightStr string
+
+	vl := i.VisitNodePolish(binary.left)
+	switch vvv := vl.(type) {
+	case int:
+		leftStr = strconv.Itoa(vvv)
+	case string:
+		leftStr = vvv
+	}
+	vr := i.VisitNodePolish(binary.right)
+	switch vvv := vr.(type) {
+	case int:
+		rightStr = strconv.Itoa(vvv)
+	case string:
+		rightStr = vvv
+	}
+	template := "%s %s %s"
+	switch binary.op.typ {
+	case Plus:
+
+		return fmt.Sprintf(template, leftStr, rightStr, "+")
+	case Minus:
+		return fmt.Sprintf(template, leftStr, rightStr, "-")
+	case Mul:
+		return fmt.Sprintf(template, leftStr, rightStr, "*")
+	case Div:
+		return fmt.Sprintf(template, leftStr, rightStr, "/")
+	default:
+		panic("AAA")
 	}
 }
