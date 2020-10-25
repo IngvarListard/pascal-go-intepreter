d9e844089d4e4116a6fcf27f74059c41f111f677
IngvarListard <vlzvoice@ya.ru>
2020-10-25 21:05:40 +0300

    feature: procedure symbol

--- pkg/calc5/symbols.go
+++ pkg/calc5/symbols.go
@@ -27,3 +27,17 @@ func (v *varSymbol) Name() string { return v.name }
 func (v *varSymbol) Type() Symbol { return v.typ }
 
 func (v *varSymbol) String() string { return fmt.Sprintf("<%v:%v>", v.name, v.typ) }
+
+type procedureSymbol struct {
+	name   string
+	params []*param
+	typ    Symbol
+}
+
+func (p *procedureSymbol) String() string {
+	return fmt.Sprintf("<procedureSymbol{%s, %v}>", p.name, p.params)
+}
+
+func (p *procedureSymbol) Name() string { return p.name }
+
+func (p *procedureSymbol) Type() Symbol { return p.typ }
